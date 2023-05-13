6eb760f92a04abcc65ea6c34a070f52eec7e1ade
TheM8thy <TheM8thy@proton.me>
2023-05-13 02:01:44 +0200

    added threading

--- fapi.py
+++ fapi.py
@@ -3,6 +3,9 @@
 import argparse
 import sys
 import requests
+import threading
+
+thread_responses = []
 
 def validatemethod(value):
     r"""Validates the methods passed into the script via the -m parameter
@@ -30,6 +33,15 @@ def error(message):
 
     print(message)
     sys.exit()
+
+def chunk_list(list_to_chunk : list, num_chunks : int):
+    r"""Splits a list into a given number of chunks up
+
+    :param list_to_chunk: The list to be chunked.
+    :param num_chunks: The number of chunks.
+    """
+    return [list_to_chunk[x:x+len(list_to_chunk)//num_chunks if len(list_to_chunk)//num_chunks > 1 else x+num_chunks] for x in range(0, len(list_to_chunk), len(list_to_chunk)//num_chunks if len(list_to_chunk)//num_chunks > 1 else num_chunks)]
+
 
 def process_response(request, match_string, default_testing_length, verbose):
     r"""Processes the response of the selected method and print it out
@@ -81,9 +93,23 @@ Last updated: 27/04/2023
 _____________________________________________________________________
 """)
 
+def thread_request(methods : list, url : str, endpoints : list, match_string : str, timeout : float, default_testing_length : int, verbose : bool, ignore_ssl_verification : bool, thread_num : int):
+    
+    global thread_responses
+
+    for endpoint in endpoints:
+        for method in methods:
+            try:
+                request = requests.request(method, f"{url}/{endpoint.strip()}", timeout=timeout, verify=ignore_ssl_verification)
+                thread_responses[thread_num].append(request)
+            except requests.ConnectTimeout:
+                print(f"[Verbose] {method} - Connection timed out - {url}")
+
 def fapi():
     r"""Main function
     """
+
+    global thread_responses
 
     banner()
 
@@ -96,9 +122,9 @@ def fapi():
     parser.add_argument('-t', '--timeout', metavar='<seconds>', type=float, help='Specify the amount of seconds before a request times out, defaults to 5', default=5)
     parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode')
     parser.add_argument('-k', '--ignore_certificates', action='store_false', help='Ignore SSL certificate verification.')
-    #group = parser.add_mutually_exclusive_group(required=True)
-    #group.add_argument('-th', '--threading', metavar='<threads>', type=int, help='Specify number of threads to use.', nargs=1)
-    #group.add_argument('-mp', '--multiprocessing', metavar='<processes>', type=int, help='Specify number of processes to use.', nargs=1)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-th', '--threading', metavar='<threads>', type=int, help='Specify number of threads to use.', nargs=1)
+    group.add_argument('-mp', '--multiprocessing', metavar='<processes>', type=int, help='Specify number of processes to use.', nargs=1)
     parser.add_argument('--version', action='version', help='Show %(prog)s version number', version='%(prog)s 0.1')
     parser._actions[0].help='Display the help options.' # pylint: disable=W0212
     args = parser.parse_args()
@@ -107,10 +133,11 @@ def fapi():
     wordlist = args.wordlist[0]
     methods : list = args.method[0]
     default_testing_length : int = args.default_testing_length[0] if isinstance(args.default_testing_length,list) else args.default_testing_length
-    ignore_ssl_verification : bool = args.ignore_certificates
     match_string : list = args.match_string
-    verbose : bool = args.verbose
     timeout : float = args.timeout
+    verbose : bool = args.verbose
+    ignore_certificates : bool = args.ignore_certificates
+    thread_num : int = args.threading
 
     print(f"URL: {','.join(urls)}")
     print(f"Methods: {','.join([x.upper() for x in methods])}")
@@ -121,14 +148,28 @@ def fapi():
 
     endpoints = wordlist.readlines()
 
-    if args.verbose:
+    if verbose:
         print("Length - URL\n")
     else:
         print("\n")
 
-    for url in urls:
-        for endpoint in [x for x in endpoints if x[0].strip() not in ['#','','/']]:
-            request_url = f"{url}/{endpoint.strip()}"
-            prepare_request(methods, request_url, match_string, timeout, default_testing_length, verbose, ignore_ssl_verification)
+    endpoints = [x for x in endpoints if x[0].strip() not in ['#','','/']]
+
+    print(args)
+
+    if thread_num:
+        threads = []
+        thread_responses = [ [] for _ in range(thread_num[0]) ]
+        chunked_list = chunk_list(endpoints, thread_num[0])
+        for current_thread in range(thread_num[0]):
+            thread = threading.Thread(target=thread_request, args=(methods, urls[0], chunked_list[current_thread], match_string, timeout, default_testing_length, verbose, ignore_certificates, current_thread))
+            threads.append(thread)
+            thread.start()
+    else:
+        for url in urls:
+            for endpoint in endpoints:
+                request_url = f"{url}/{endpoint.strip()}"
+                prepare_request(methods, request_url, match_string, timeout, default_testing_length, verbose, ignore_certificates)
 
 fapi()
+print(thread_responses)
